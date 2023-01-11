d4edd5c6e664d4d5aa24405e9ab8249d239da513
Anton Zaitsev <zaitsev.anton@outlook.com>
2023-01-11 18:01:27 +0100

    [FEATURE ] Display Website generation (#100)
    * [FEATURE] dynamic page generation with dummy data
    
    * [TASK] Prepare for API integration

--- backend/display_service/src/app.js
+++ backend/display_service/src/app.js
@@ -1,11 +1,35 @@
 const redisPubSub = require('./redisPubSub')
 
 const express = require("express");
-
 const app = express();
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
 
-app.get("/", (req, res) => {
-  res.sendFile("index.html", { root: __dirname });
+const { fetchData } = require("./helpers/fetchData");
+
+app.get("/display/campus/:campus/rooms/:room", (req, res) => {
+  const campusParam = req.params.campus;
+  const roomParam = req.params.room;
+
+  console.log(campusParam);
+  console.log(roomParam);
+
+  // fetch room information data from data_service for room
+  fetchData("http://data_service:8000/api/campus").then((data) => {
+    const campus = data.find((campus) => campus.name === "Gummersbach");
+    let roomInfo;
+    campus.buildings.forEach((building) => {
+      building.rooms.forEach((room) => {
+        if (room.roomNo === roomParam) {
+          roomInfo = room;
+        }
+      });
+    });
+  });
+
+  console.log(roomInfo);
+
+  // return res.render("pages/index", { timetable: data3216 });
 });
 
 const PORT = process.env.PORT || 8001;
