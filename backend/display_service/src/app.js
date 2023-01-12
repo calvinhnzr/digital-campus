6c151be0a335d19ebe9174c0293325b38e35a987
Anton Zaitsev <zaitsev.anton@outlook.com>
2023-01-12 14:22:27 +0100

    [TASK] Display generation finalized (#105)
    * [TASK] Add api integration
    
    * [TASK] Specify index text
    
    * [TASK] Refactor display generation

--- backend/display_service/src/app.js
+++ backend/display_service/src/app.js
@@ -1,38 +1,57 @@
-const redisPubSub = require('./redisPubSub')
+// redis
+const redisPubSub = require("./redisPubSub");
 
+// express
 const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 
-const { fetchData } = require("./helpers/fetchData");
-
-app.get("/display/campus/:campus/rooms/:room", (req, res) => {
+// example: http://localhost:8001/display/campus/gummersbach/rooms/3216
+// room display endpoint
+app.get("/display/campus/:campus/rooms/:room", async (req, res) => {
   const campusParam = req.params.campus;
   const roomParam = req.params.room;
 
-  console.log(campusParam);
-  console.log(roomParam);
+  // get room information
+  // -------------------------
+  const roomResponse = await fetch("http://data_service:8000/api/campus");
+  const roomData = await roomResponse.json();
 
-  // fetch room information data from data_service for room
-  fetchData("http://data_service:8000/api/campus").then((data) => {
-    const campus = data.find((campus) => campus.name === "Gummersbach");
-    let roomInfo;
-    campus.buildings.forEach((building) => {
-      building.rooms.forEach((room) => {
-        if (room.roomNo === roomParam) {
+  let roomInfo;
+  const rCampus = roomData.find((campus) => campus.name.toLowerCase() === campusParam.toLowerCase());
+  rCampus.buildings.forEach((building) => {
+    building.floors.forEach((floor) => {
+      floor.rooms.forEach((room) => {
+        if (room.number.toLowerCase() === roomParam.toLowerCase()) {
           roomInfo = room;
         }
       });
     });
   });
 
-  console.log(roomInfo);
+  if (!roomInfo) {
+    return res.status(404).send("Room not found");
+  }
+  // -------------------------
 
-  // return res.render("pages/index", { timetable: data3216 });
+  // get timetable information
+  // -------------------------
+  const timetableResponse = await fetch("http://data_service:8000/api/timetables");
+  const timetableData = await timetableResponse.json();
+
+  const tCampus = timetableData.find((campus) => campus.campus.toLowerCase() === campusParam.toLowerCase());
+  const timetable = tCampus.rooms.find((room) => room.roomNo.toLowerCase() === roomParam.toLowerCase());
+
+  if (!timetable) {
+    return res.status(404).send("Timetable not found");
+  }
+  // -------------------------
+
+  return res.render("pages/index", { timetable, roomInfo });
 });
 
 const PORT = process.env.PORT || 8001;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
-redisPubSub.startRedis()
+redisPubSub.startRedis();
